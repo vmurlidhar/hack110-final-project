6d25f4cb9ba292b966d5901bdbfe2a949701ce05
Varun Murlidhar <vmurlidhar@unc.edu>
2021-11-13 01:52:11 -0500

    Progress on Final Project

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request
+from helpers import count_points, most_answered, find_villain, user
+
+app: Flask = Flask(__name__)
+users: list[user] = []
+user_number: int = 0
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route('/quiz', methods=["GET", "POST"])
+def quiz():
+    if request.method == "POST":
+        global users
+        global user_number
+
+        fname: str = request.form['fname']
+        lname: str = request.form['lname']
+        pop_song: str = request.form['favorite pop song']
+        rap_song: str = request.form['favorite rap song']
+        throwback_song: str = request.form['favorite throwback song']
+        disney_song: str = request.form['favorite disney song']
+        musical_song: str = request.form['favorite musical song']
+        christmas_song: str = request.form['favorite christmas song']
+        classical_song: str = request.form['favorite classical song']
+        international_song: str = request.form['favorite international song']
+        answer_choices: list[str] = [pop_song, rap_song, throwback_song, disney_song, musical_song, christmas_song, classical_song, international_song]
+
+        if fname == '' or lname == '':
+            return render_template("quiz.html")
+
+        total_points: dict[str, int] = count_points(answer_choices)
+        most_chosen_answer: str = most_answered(total_points)
+        villain: str = find_villain(most_chosen_answer)
+        new_user: user = user(user_number, fname, lname, villain)
+        users.append(new_user)
+
+        user_number += 1
+
+        return render_template("result.html", villain=villain)
+    return render_template("quiz.html")
+
+
+@app.route('/all-results')
+def all_results():
+    return render_template('all-results.html', users=users)
+
+
+@app.route('/user<usernumber>')
+def display_user(usernumber: str):
+    return render_template('user.html', user=users[int(usernumber)])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ helpers.py
@@ -0,0 +1,48 @@
+def count_points(compiled_list: list[str]) -> dict[str, int]:
+    result: dict[str, int] = {"1": 0, "2": 0, "3": 0, "4": 0}
+
+    for item in compiled_list:
+        if item == "1":
+            result["1"] += 1
+        elif item == "2":
+            result["2"] += 1
+        elif item == "3":
+            result["3"] += 1
+        else:
+            result["4"] += 1
+
+    return result
+
+def most_answered(dictionary: dict[str, int]) -> str:
+    checker: int = 0
+    most_chosen_choice: str = ""
+
+    for item in dictionary:
+        if dictionary[item] > checker:
+            checker = dictionary[item]
+            most_chosen_choice = item
+    
+    return most_chosen_choice
+
+
+def find_villain(most_chosen_choice: str) -> str:
+    if most_chosen_choice == '1':
+        return 'Scar'
+    elif most_chosen_choice == '2':
+        return 'Maleficent'
+    elif most_chosen_choice == '3':
+        return 'Jafar'
+    else:
+        return 'Ursula'
+
+class user:
+    id: int
+    first_name: str
+    last_name: str
+    house: str
+
+    def __init__(self, id: int, fname: str, lname: str, house: str):
+        self.id = id
+        self.first_name = fname
+        self.last_name = lname
+        self.house = house
